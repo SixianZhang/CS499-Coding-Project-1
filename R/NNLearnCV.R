0da638438b9710dea0746e028f044865b60f9ecf
SixianZhang <43683141+SixianZhang@users.noreply.github.com>
2019-02-08 17:53:19 -0700

    Adding R functions

---
+++ R/NNLearnCV.R
@@ -0,0 +1,95 @@
+#' Cross-validation using nearest neighbors
+#'
+#' Using nearest neighbors algorithm for cross validation to find out the best K and give a prediction of the test data based on it.
+#'
+#' @param X.mat numeric train feature matrix [n x p]
+#' @param y.vec numeric train label vector [n x 1], 0/1 for binary classification, real number for regression.
+#' @param max.neighbors scalar integer, max number of the neighbors
+#' @param fold.vec integer vector that holds fold ID number [n x 1]
+#' @param n.folds scalar integer, number of the folds
+#'
+#' @return
+#' @export
+#'
+#' @examples
+NNLearnCV <-
+  function(X.mat,
+           y.vec,
+           max.neighbors = 30L,
+           fold.vec = NULL,
+           n.folds = 5L) {
+    # Check type and dimension
+    if (!all(is.matrix(X.mat), is.numeric(X.mat))) {
+      stop("X.mat must be a numeric matrix")
+    }
+    
+    if (!all(is.vector(y.vec),
+             is.numeric(y.vec),
+             length(y.vec) == nrow(X.mat))) {
+      stop("y.vec must be a numeric vector with the size of nrow(X.mat)")
+    }
+    
+    if (!all(is.integer(max.neighbors), length(max.neighbors) == 1)) {
+      stop("max.neighbors must be an integer scalar")
+    }
+    
+    if (!all(
+      !is.null(fold.vec),
+      is.vector(fold.vec),
+      is.numeric(fold.vec),
+      length(fold.vec) == length(y.vec)
+    )) {
+      stop("fold.vec must be a vector with the length of length(y.vec)")
+    }
+    
+    if (!all(is.integer(n.folds), length(n.fold) == 1)) {
+      stop("n.fold must be an integer scalar")
+    }
+    
+    # Assign random fold sequence if it is null
+    if (is.null(fold.vec)) {
+      fold.vec <- sample(rep(1:n.folds, l = nrow(X.mat)))
+    }
+    
+    label.is.binary = (y.vec == 0) || (y.vec == 1)
+    
+    
+    #
+    for (fold.i in seq_len(n.fold)) {
+      for (prediction.set.name in c("train", "validation")) {
+        train.index <- which(fold.vec != fold.i)
+        if (prediction.set.name == "train") {
+          validation.index = which(fold.vec != fold.i)
+        } else{
+          validation.index = which(fold.vec == fold.i)
+        }
+        
+        predict.mat <-
+          NN1toKmaxPredict(X.mat[train.index, ], y.vec[train.index], X.mat[validation.index,], max.neighbors)
+        
+        loss.mat <- if (label.is.binary) {
+          ifelse(predict.mat > 0.5, 1, 0) != y.vec[validatioin.index]
+        }else{
+          (pred.mat - y.vec[validation.index])^2
+        }
+        if(prediction.set.name == "train"){
+          train.loss.mat[, fold.i] <- colMeans(loss.mat)
+        }else{
+          validation.loss.mat[, fold.i] <- colMeans(loss.mat)
+        }
+      }
+    }
+    
+    train.loss.vec <- rowMeans(train.loss.mat)
+    validation.loss.vec <- rowMeans(validation.loss.mat)
+    selected.neighbors <- min(validation.loss.vec)
+    predict <- function(feature.mat){
+      
+      
+      return(prediction.vec)
+    }
+    
+    
+    
+    
+  }
--- test/TestScript.R
+++ test/TestScript.R
@@ -1,19 +1,25 @@
 data(zip.train, package = "ElemStatLearn")
 X <- zip.train[1:5, -1]
 y <- zip.train[1:5, 1]
-Xtest <- zip.train[6, -1]
-ytest <- zip.train[6, 1]
-max.neighbor <- 3
+Xtest <- matrix(zip.train[6:7, -1], ncol = ncol(X))
+ytest <- (zip.train[6, 1])
+max.neighbor <- 3L
 ypredict <- 0
-.C(
-  "NN1toKmaxPredict_interface",
-  as.integer(nrow(X)),
-  as.integer(nrow(Xtest)),
-  as.integer(ncol(X)),
-  as.integer(max.neighbor),
-  as.double(X),
-  as.double(y),
-  as.double(Xtest),
-  as.double(ypredict)
-)
+# .C(
+#   "NN1toKmaxPredict_interface",
+#   as.integer(nrow(X)),
+#   as.integer(nrow(Xtest)),
+#   as.integer(ncol(X)),
+#   as.integer(max.neighbor)
+#   as.double(X),
+#   as.double(y),
+#   as.double(Xtest),
+#   as.double(ypredict)
+# )
+list <- NN1toKmaxPredict(X,y,Xtest,max.neighbor)
 
+someFun <- function(){
+  return(456)
+}
+
+list <- list(1,50,sf = someFun)

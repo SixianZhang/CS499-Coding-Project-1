fd27b4dd039f61d7cffd9694f7c264fdc1d438d0
SixianZhang <43683141+SixianZhang@users.noreply.github.com>
2019-02-15 18:24:11 -0700

    baseline regression test script

--- tests/TestScript.R
+++ tests/TestScript.R
@@ -1,3 +1,15 @@
+BaselinePredict <- function(y.vec,fold.vec,n.folds){
+  prediction <- rep(NA,n.folds)
+  prediction.loss <- rep(NA,n.folds)
+  for (fold.i in seq_len(n.folds)){
+    train.index <- which(fold.vec != fold.i)
+    validation.index <- which(fold.vec == fold.i)
+    prediction[fold.i] <- mean(y.vec[train.index])
+    prediction.loss[fold.i] <- mean((y.vec[validation.index] - prediction[fold.i])^2)
+  }
+  return(prediction.loss)
+}
+
 
 #Data 1: spam
 data(spam, package = "ElemStatLearn")
@@ -7,54 +19,65 @@ y.vec = spam$spam
 levels(y.vec) <- c(0,1)
 y.vec <- as.double(as.vector(y.vec))
 
-testX.mat = X.mat[c(1,nrow(X.mat)),]
-max.neighbors = 5L
+n.folds <- 3L
+fold.vec <- sample(rep(1:n.folds, l = nrow(X.mat)))
+
+NN.pred.mat <- NNLearnCV(X.mat = X.mat, y.vec = y.vec, fold.vec = fold.vec, n.folds = n.folds)
+NN.loss.vec <- colMeans(NN.pred.mat$validation.loss.mat)
+baseLine.loss.vec <- BaselinePredict(y.vec,fold.vec,n.folds)
+t(cbind(NN.loss.vec,baseLine.loss.vec))
 
 
-#X.mat: double matrix; y.vec: double vector; testX.mat: samller double matrix
-C.pred.mat <-
-  NN1toKmaxPredict(X.mat, y.vec, testX.mat, max.neighbors)
-C.pred.mat
-
-C.pred.model <- NNLearnCV(X.mat,y.vec,max.neighbors,NULL,3L)
-C.pred.model$predict(testX.mat)
-y.vec[c(1,nrow(X.mat))]
-
-#Data 2: SAheart
-rm(list = ls())
-data(SAheart, package = "ElemStatLearn")
-X.mat2 <- data.matrix(subset(SAheart, select = -10))
-
-y.vec2 <- SAheart[,10]
-y.vec2 <- as.double(as.vector(y.vec2))
-
-testX.mat2 <- X.mat2[c(1, nrow(X.mat2)-1),]
-max.neighbors = 5L
-
-C.pred.mat2 <-
-  NN1toKmaxPredict(X.mat2, y.vec2, testX.mat2, max.neighbors)
-C.pred.mat2
-
-C.pred.model2 <- NNLearnCV(X.mat2,y.vec2,max.neighbors,NULL,3L)
-C.pred.model2$predict(testX.mat2)
-y.vec2[c(1,nrow(X.mat2)-1)]
-
-#Data 3: zip.train
-rm(list = ls())
-data(zip.train, package = "ElemStatLearn")
-entire.mat <- data.matrix(zip.train)
-y.vec3 <- entire.mat[,1]
-
-binary.index <- which((y.vec3 == 1) | (y.vec3 == 0))
-X.mat3 <- entire.mat[binary.index,]
-y.vec3 <- y.vec3[binary.index]
-testX.mat3 <- X.mat3[c(5,nrow(X.mat3)-7),]
-max.neighbors = 5L
-
-C.pred.mat3 <-
-  NN1toKmaxPredict(X.mat3, y.vec3, testX.mat3, max.neighbors)
-C.pred.mat3
-
-C.pred.model3 <- NNLearnCV(X.mat3,y.vec3,max.neighbors,NULL,3L)
-C.pred.model3$predict(testX.mat3)
-y.vec3[c(5,nrow(X.mat3)-7)]
+# 
+# #X.mat: double matrix; y.vec: double vector; testX.mat: samller double matrix
+# C.pred.mat <-
+#   NN1toKmaxPredict(X.mat, y.vec, testX.mat, max.neighbors)
+# C.pred.mat
+# 
+# C.pred.model <- NNLearnCV(X.mat,y.vec,max.neighbors,NULL,3L)
+# C.pred.model$predict(testX.mat)
+# y.vec[c(1,nrow(X.mat))]
+# 
+# 
+# 
+# 
+# 
+# 
+# #Data 2: SAheart
+# rm(list = ls())
+# data(SAheart, package = "ElemStatLearn")
+# X.mat2 <- data.matrix(subset(SAheart, select = -10))
+# 
+# y.vec2 <- SAheart[,10]
+# y.vec2 <- as.double(as.vector(y.vec2))
+# 
+# testX.mat2 <- X.mat2[c(1, nrow(X.mat2)-1),]
+# max.neighbors = 5L
+# 
+# C.pred.mat2 <-
+#   NN1toKmaxPredict(X.mat2, y.vec2, testX.mat2, max.neighbors)
+# C.pred.mat2
+# 
+# C.pred.model2 <- NNLearnCV(X.mat2,y.vec2,max.neighbors,NULL,3L)
+# C.pred.model2$predict(testX.mat2)
+# y.vec2[c(1,nrow(X.mat2)-1)]
+# 
+# #Data 3: zip.train
+# rm(list = ls())
+# data(zip.train, package = "ElemStatLearn")
+# entire.mat <- data.matrix(zip.train)
+# y.vec3 <- entire.mat[,1]
+# 
+# binary.index <- which((y.vec3 == 1) | (y.vec3 == 0))
+# X.mat3 <- entire.mat[binary.index,]
+# y.vec3 <- y.vec3[binary.index]
+# testX.mat3 <- X.mat3[c(5,nrow(X.mat3)-7),]
+# max.neighbors = 5L
+# 
+# C.pred.mat3 <-
+#   NN1toKmaxPredict(X.mat3, y.vec3, testX.mat3, max.neighbors)
+# C.pred.mat3
+# 
+# C.pred.model3 <- NNLearnCV(X.mat3,y.vec3,max.neighbors,NULL,3L)
+# C.pred.model3$predict(testX.mat3)
+# y.vec3[c(5,nrow(X.mat3)-7)]
--- tests/regressionTestScript.R
+++ tests/regressionTestScript.R
@@ -1,34 +1,48 @@
+BaselinePredict <- function(y.vec,fold.vec,n.folds){
+  prediction <- rep(NA,n.folds)
+  prediction.loss <- rep(NA,n.folds)
+  for (fold.i in seq_len(n.folds)){
+    train.index <- which(fold.vec != fold.i)
+    validation.index <- which(fold.vec == fold.i)
+    prediction[fold.i] <- mean(y.vec[train.index])
+    prediction.loss[fold.i] <- mean((y.vec[validation.index] - prediction[fold.i])^2)
+  }
+  return(prediction.loss)
+}
+
+
 # Data 4: prostate
 
 data(prostate, package = "ElemStatLearn")
 X.mat = data.matrix(subset(prostate,select = -c(lpsa,train)))
-y.vec = prostate$lpsa
+y.vec = as.vector(prostate$lpsa)
+n.folds <- 3L
+fold.vec <- sample(rep(1:n.folds, l = nrow(X.mat)))
 
-testX.mat = X.mat[c(1,2,7,10,nrow(X.mat)),]
-max.neighbors = 5L
+NN.pred.mat <- NNLearnCV(X.mat = X.mat, y.vec = y.vec, fold.vec = fold.vec, n.folds = n.folds)
+NN.loss.vec <- NN.pred.mat$train.loss.mat[NN.pred.mat$selected.neighbors,]
 
-C.pred.mat <-
-  NN1toKmaxPredict(X.mat, y.vec, testX.mat, max.neighbors)
-C.pred.mat
-
-C.pred.model <- NNLearnCV(X.mat,y.vec,max.neighbors,NULL,3L)
-C.pred.model$predict(testX.mat)
-y.vec[c(1,2,7,10,nrow(X.mat))]
-
-
-#Data 5: ozone 
-rm(list = ls())
+baseline.loss.vec <- BaselinePredict(y.vec,fold.vec,n.folds)
+result <- t(cbind(NN.loss.vec,baseline.loss.vec))
+rownames(result) <- c("Nearest Neighbors","Baseline")
+colnames(result) <- c("Fold 1","Fold 2","Fold 3")
+result
+barplot(result,main = "Regression: prostate",xlab = "mean loss value",legend = (rownames(result)),beside = TRUE)
+plot()
+# #Data 5: ozone 
+# rm(list = ls())
 data(ozone, package = "ElemStatLearn")
-X.mat = data.matrix(subset(ozone,select = -1))
-y.vec = ozone[,ncol(ozone)]
+X.mat = data.matrix(subset(ozone,select = -c(ozone)))
+y.vec = as.vector(ozone$ozone)
+n.folds <- 3L
+fold.vec <- sample(rep(1:n.folds, l = nrow(X.mat)))
 
-testX.mat = X.mat[c(1,2,7,10,nrow(X.mat)),]
-max.neighbors = 5L
+NN.pred.mat <- NNLearnCV(X.mat = X.mat, y.vec = y.vec, fold.vec = fold.vec, n.folds = n.folds)
+NN.loss.vec <- NN.pred.mat$train.loss.mat[NN.pred.mat$selected.neighbors,]
 
-C.pred.mat <-
-  NN1toKmaxPredict(X.mat, y.vec, testX.mat, max.neighbors)
-C.pred.mat
-
-C.pred.model <- NNLearnCV(X.mat,y.vec,max.neighbors,NULL,3L)
-C.pred.model$predict(testX.mat)
-y.vec[c(1,2,7,10,nrow(X.mat))]
+baseline.loss.vec <- BaselinePredict(y.vec,fold.vec,n.folds)
+result <- t(cbind(NN.loss.vec,baseline.loss.vec))
+rownames(result) <- c("Nearest Neighbors","Baseline")
+colnames(result) <- c("Fold 1","Fold 2","Fold 3")
+result
+barplot(result,main = "Regression: ozone", xlab = "mean loss value",legend = (rownames(result)),beside = TRUE)
